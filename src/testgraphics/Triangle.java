27054afced7007b6a6d75c7cce952dbde4a5860b
¨YaelAnaya¨ <yael.anaya@uabc.edu.mx>
2021-09-21 21:44:25 -0700

    version final, corregida la clase triangulo ya compara si está fuera de los limites.

--- src/testgraphics/Triangle.java
+++ src/testgraphics/Triangle.java
@@ -17,17 +17,16 @@ public class Triangle extends Figure {
      * Estos atributos guardan los valores de los puntos X e Y que necesitamos,
      * para dibujar un poligono con la funcion drawPolygon de la clase Grapichs.
      */
-    private int[] xPoints;
-    private int[] yPoints;
+    private int[] xPoints = new int[3];;
+    private int[] yPoints = new int[3];;
 
     /**
      * Este constrcutor es utilizado para crear al Triangulo.
      */
     public Triangle(int xPosition, int yPosition, int size, Color color) {
-        super(xPosition, yPosition, size, color);
-        xPoints = new int[3];
-        yPoints = new int[3];
-
+        super(xPosition, yPosition, size, color); 
+        getXPointsTriangle();
+        getYPointsTriangle();
     }
 
     /**
@@ -36,14 +35,13 @@ public class Triangle extends Figure {
      *
      * @return el vector con los valores de los puntos de X
      */
-    public int[] getXPointsTriangle() {
+    public void getXPointsTriangle() {
         //Guardamos el punto incial X.
         xPoints[0] = xPosition;
         //Recorremos el punto inicial la mitad del tamaño del lado a la derecha.
         xPoints[1] = xPosition + (size / 2);
         //Recorremos el punto inicial la mitad del tamaño del lado a la izquierda.
         xPoints[2] = xPosition - (size / 2);
-        return xPoints;
     }
 
     /**
@@ -52,13 +50,12 @@ public class Triangle extends Figure {
      *
      * @return el vector con los valores de los puntos de Y
      */
-    public int[] getYPointsTriangle() {
+    public void getYPointsTriangle() {
         //Guardamos el punto incial Y.
         yPoints[0] = yPosition;
         //Calculamos la altura para los lados, gracias al teorema de pitagoras.
         yPoints[1] = (int) (yPosition + Math.sqrt(Math.pow(size, 2) - Math.pow(size / 2, 2)));
         yPoints[2] = yPoints[1];
-        return yPoints;
     }
 
     /**
@@ -70,7 +67,7 @@ public class Triangle extends Figure {
     @Override
     public void drawFigure(Graphics g) {
         g.setColor(color);
-        g.fillPolygon(getXPointsTriangle(), getYPointsTriangle(), 3);
+        g.fillPolygon(xPoints, yPoints, 3);
     }
 
     @Override
@@ -80,8 +77,9 @@ public class Triangle extends Figure {
         edgeX = xPoints[1];
         edgeX2 = xPoints[2];
         edgeY = yPoints[1];
+  
 
-        return edgeX > xLimit || edgeY > yLimit || edgeX2 < 0;
+        return edgeX > xLimit || edgeY > yLimit || edgeX2 < 0 ;
     }
 
 }
